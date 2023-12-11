3cd111cf8ed8ccccba59ada949cb952cdddf77f1
Bojin Li <bojinxx@gmail.com>
2023-12-11 18:36:13 +0800

    feat: add default outbound IP detection

--- example/CLI/main.go
+++ example/CLI/main.go
@@ -5,14 +5,25 @@ import (
 	"flag"
 	"fmt"
 	"natdetection"
+	"net"
 )
 
 func main() {
+	// Get Default Outbound IP
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
 	// Set CLI flags
 	jsonOutput := flag.Bool("json", false, "JSON output")
 	stunHost := flag.String("stun-host", "", "STUN host to use")
 	stunPort := flag.Int("stun-port", natdetection.Defaults["stun_port"].(int), "STUN host port to use")
-	sourceIP := flag.String("source-ip", natdetection.Defaults["source_ip"].(string), "network interface for client")
+	// sourceIP := flag.String("source-ip", natdetection.Defaults["source_ip"].(string), "network interface for client")
+	sourceIP := flag.String("source-ip", localAddr.IP.String(), "network interface for client")
 	sourcePort := flag.Int("source-port", natdetection.Defaults["source_port"].(int), "port to listen on for client")
 	version := flag.Bool("version", false, "show version")
 
@@ -24,6 +35,8 @@ func main() {
 		return
 	}
 
+	fmt.Println("Default Outbound IP:", localAddr.IP)
+	fmt.Println("Tested Source IP:", *sourceIP)
 	fmt.Println("- Discovering NAT type (it may take 5 to 60 seconds) ...")
 
 	// Get NAT type
